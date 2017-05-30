91d9c084a47f8ef8f6291c76349d03af6ac472f4
Joan <joanpecamp1993@gmail.com>
2017-05-30 17:35:11 +0200

    Actualizacion 3

---
+++ BananaGest/src/banana/com/models/Tarea.java
@@ -0,0 +1,10 @@
+package banana.com.models;
+
+public class Tarea {
+	String descripcion;
+	String notas;
+	String fechaI;
+	String fechaF;
+	String responsable;
+	String estado;
+}
--- BananaGest/src/banana/com/models/Usuario.java
+++ BananaGest/src/banana/com/models/Usuario.java
@@ -1,5 +1,7 @@
 package banana.com.models;
 
 public class Usuario {
-
+	String name;
+	String email;
+	String password;
 }
