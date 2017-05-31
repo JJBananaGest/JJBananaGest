8ea21dba5716c0269ff8e66d46c958cb47c1f354
Joan <joanpecamp1993@gmail.com>
2017-05-31 15:21:03 +0200

    Actu!

--- BananaGest/src/banana/com/models/Tarea.java
+++ BananaGest/src/banana/com/models/Tarea.java
@@ -5,15 +5,13 @@ public class Tarea {
 	String notas;
 	String fechaI;
 	String fechaF;
-	String responsable;
 	String estado;
-	public Tarea(String descripcion, String notas, String fechaI, String fechaF, String responsable, String estado) {
+	public Tarea(String descripcion, String notas, String fechaI, String fechaF,  String estado) {
 		super();
 		this.descripcion = descripcion;
 		this.notas = notas;
 		this.fechaI = fechaI;
 		this.fechaF = fechaF;
-		this.responsable = responsable;
 		this.estado = estado;
 	}
 	
